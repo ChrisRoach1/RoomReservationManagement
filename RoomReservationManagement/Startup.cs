df255d5461834e80ec1d0ee3332e9dd3e02cabaa
Christopher Roach <c.roach10@yahoo.com>
2019-02-17 15:15:34 -0500

    first

---
+++ RoomReservationManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RoomReservationManagement.Startup))]
+namespace RoomReservationManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

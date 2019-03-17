8193c790a2e0cadf63f66485e5dd3fec0e1976d5
root <root@LilRyuko.localdomain>
2019-03-16 20:20:41 -0400

    added panolensJS/three.js, Rooms controller and index view

---
+++ RoomReservationManagement/Controllers/RoomsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RoomReservationManagement.Models;
+
+namespace RoomReservationManagement.Controllers
+{
+    public class RoomsController : Controller
+    {
+        public ApplicationDbContext databaseConnection = new ApplicationDbContext();
+
+        // GET: Rooms
+        public ActionResult Index()
+        {
+            var roomData = databaseConnection.Res_Rooms;
+            return View(roomData);
+        }
+    }
+}

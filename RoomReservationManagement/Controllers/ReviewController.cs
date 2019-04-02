5831be955e7c311189d53eea10aa491a3df52383
flyingpig86 <flyingpig86@yahoo.com>
2019-04-01 20:05:26 -0400

    Room Review Controllers and Views.

---
+++ RoomReservationManagement/Controllers/ReviewController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RoomReservationManagement.Models;
+
+namespace RoomReservationManagement.Controllers
+{
+    public class ReviewController : Controller
+    {
+        public DataOperations dataOps = new DataOperations();
+
+        // GET: Rooms
+        public ActionResult Index()
+        {
+            var roomData = dataOps.getAllRooms();
+            return View(roomData);
+        }
+
+        public ActionResult Details()
+        {
+            var roomData = dataOps.getAllRooms();
+            return View(roomData);
+        }
+
+        public ActionResult TestSandbox()
+        {
+            var roomData = dataOps.getAllRooms();
+            return View(roomData);
+        }
+    }
+}

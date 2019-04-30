54a680b094123d778700e44ecc6a971b4178144f
flyingpig86 <flyingpig86@yahoo.com>
2019-04-29 21:55:53 -0400

    Functionality update and implemented security checks. Again....

--- RoomReservationManagement/Controllers/ReviewController.cs
+++ RoomReservationManagement/Controllers/ReviewController.cs
@@ -14,14 +14,69 @@ namespace RoomReservationManagement.Controllers
 {
     public class ReviewController : Controller
     {
+        SecurityCheck secCheck = new SecurityCheck();
         public DataOperations dataOps = new DataOperations();
 
 
         //GET: Reviews
         public ActionResult Index()
         {
-            var revData = dataOps.getAllReviews();
-            return View(revData);
+            if (secCheck.hasFullAccess())
+            {
+                ViewBag.errorMessage = "";
+                ViewBag.successValue = false;
+                var revData = dataOps.getAllReviews();
+                return View(revData);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        public ActionResult Details()
+        {
+            if (secCheck.hasFullAccess())
+            {
+                ViewBag.errorMessage = "";
+                ViewBag.successValue = false;
+                var revData = dataOps.getAllReviews();
+                return View(revData);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        public ActionResult NewTest()
+        {
+            if (secCheck.hasFullAccess())
+            {
+                ViewBag.errorMessage = "";
+                ViewBag.successValue = false;
+                var revData = dataOps.getAllReviews();
+                return View(revData);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        public ActionResult NewReview()
+        {
+            if (secCheck.hasFullAccess())
+            {
+                ViewBag.errorMessage = "";
+                ViewBag.successValue = false;
+                var revData = dataOps.getAllReviews();
+                return View(revData);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
         }
 
     }
